3a12591a6bdbfca1bfdfe710097328939bcb01b4
Alex Johnson <ajohnson2389@gmail.com>
2020-10-13 17:25:39 -0400

    updated to parse json data to give message based on location

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:clima/screens/loading_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() => runApp(MyApp());
 
